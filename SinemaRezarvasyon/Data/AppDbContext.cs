3a736a65214b2d2ee37b03b2479bfd13afe448bc
ErenSevindik1 <133363009+ErenSevindik1@users.noreply.github.com>
2024-04-20 23:03:39 +0300

    Proje dosyası ekle.

---
+++ SinemaRezarvasyon/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RezervationErenSevindik.Models;
+using RezervationErenSevindik.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RezervationErenSevindik.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=..\\..\\files\\RezervationErenSevindikDb.db");
+        }
+
+        public DbSet<Film> Film { get; set; }
+        public DbSet<Seans> Seans { get; set; }
+        public DbSet<Rezervasyon> Rezervasyon { get; set; }
+    }
+}
---
+++ SinemaRezarvasyon/Services/FilmServis.cs
@@ -0,0 +1,25 @@
+﻿using RezervationErenSevindik.Data;
+using RezervationErenSevindik.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RezervationErenSevindik.Services
+{
+    public class FilmServis
+    {
+        AppDbContext _dbContext = new AppDbContext();
+
+        public List<Film> GetAll()
+        {
+            return _dbContext.Film.ToList();
+        }
+
+        public Film GetFilmDetailsBasedOnfilmId(int filmId)
+        {
+            return _dbContext.Film.FirstOrDefault(m => m.id == filmId);
+        }
+    }
+}
